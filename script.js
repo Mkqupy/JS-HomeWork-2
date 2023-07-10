cbc203793a7d8dc110f3394665510d942bbd433e
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2023-07-10 20:46:13 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1
+let celsium = 10;
+let farenheite = celsium * 1.8+32;
+console.log(farenheite);
+
+// 2
+let days = 30;
+let hours = days * 24;
+let minutes = hours * 60;
+console.log(hours);
+console.log(minutes);
+
+// 3
+let helth = 100;
+let energy = 100;
+
+helth -= 10;
+energy -= 10;
+
+console.log(helth);
+console.log(energy);
+
+// 4
+let total = 87;
+let discount = total * 10 / 100;
+console.log(total - discount)
+
+// 5
+let number = 77.456987654;
+console.log(Math.floor(number));
+
+// 6
+let string1 = "3.8";
+console.log(parseFloat(string1));
+
+// 7
+let string2 = "38";
+console.log(parseInt(string2));
+
+// 8
+let number2 = "83";
+console.log(Math.sqrt(number2));
+
+// 9
+let number3 = 97;
+let string3 = "79";
+
+console.log(number3.toString());
+console.log(parseInt(string3));
